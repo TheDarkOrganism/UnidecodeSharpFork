67e443f46ca53fb3f29ddaedb473a27d80ae4430
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2026-01-30 08:25:19 -0800

    Add .NET 10+ extension methods for Unidecode
    Introduces conditional extension methods for string and char Unidecode functionality when targeting .NET 10.0 or greater, using preprocessor directives. This allows for more idiomatic extension method usage in newer .NET versions while maintaining compatibility with previous versions.

--- UnidecodeSharpFork/Unidecoder.cs
+++ UnidecodeSharpFork/Unidecoder.cs
@@ -56,6 +56,13 @@ namespace UnidecodeSharpFork
 			}
 		}
 
+#if NET10_0_OR_GREATER
+		#pragma warning disable IDE0055 // Fix formatting
+
+		extension(string? input)
+		{
+#endif
+
 		/// <summary>
 		/// Transliterate Unicode string to ASCII string.
 		/// </summary>
@@ -66,12 +73,29 @@ namespace UnidecodeSharpFork
 		/// ASCII string. There are [?] (3 characters) in places of some unknown(?) unicode characters.
 		/// It is this way in Python code as well.
 		/// </returns>
+#if NET10_0_OR_GREATER
+		public string Unidecode()
+#else
 		public static string Unidecode(this string? input)
+#endif
 		{
 			return string.IsNullOrEmpty(input) ? string.Empty : input.All(x => x < 0x80)
 				? input
 				: string.Join(string.Empty, input.Select(c => c < 0x80 ? c.ToString() : Characters.TryGetValue(c >> 8, out IReadOnlyList<string>? transliterations) ? transliterations[c & 0xff] : default));
 		}
+
+#if NET10_0_OR_GREATER
+		}
+
+		#pragma warning restore IDE0055 // Fix formatting
+#endif
+
+#if NET10_0_OR_GREATER
+		#pragma warning disable IDE0055 // Fix formatting
+
+		extension(char c)
+		{
+#endif
 
 		/// <summary>
 		/// Transliterate Unicode character to ASCII string.
@@ -83,11 +107,21 @@ namespace UnidecodeSharpFork
 		/// ASCII string. Unknown(?) unicode characters will return [?] (3 characters).
 		/// It is this way in Python code as well.
 		/// </returns>
+#if NET10_0_OR_GREATER
+		public string Unidecode()
+#else
 		public static string Unidecode(this char c)
+#endif
 		{
 			return c < 0x80
 				? c.ToString()
 				: Characters.TryGetValue(c >> 8, out IReadOnlyList<string>? transliterations) ? transliterations[c & 0xff] : string.Empty;
 		}
+
+#if NET10_0_OR_GREATER
+		}
+
+#pragma warning restore IDE0055 // Fix formatting
+#endif
 	}
 }
