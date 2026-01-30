5ee1185769fff62f1c567633132859a881c4be94
TheDarkOrganism <15151474+TheDarkOrganism@users.noreply.github.com>
2026-01-30 10:37:28 -0800

    Add netstandard2.0 support
    Expanded target frameworks to include netstandard2.0 and adjusted project settings to conditionally enable nullable reference types and implicit usings based on the target framework. Updated Unidecoder.cs to use conditional compilation for nullable annotations and usings, improving compatibility across frameworks.

--- UnidecodeSharpFork/Unidecoder.cs
+++ UnidecodeSharpFork/Unidecoder.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿#if !NET5_0_OR_GREATER
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+#endif
 using System.Reflection;
 using System.Text.Json;
 
@@ -14,7 +16,11 @@ namespace UnidecodeSharpFork
 		private const string _unicodeFile = "Unicode.json";
 
 #if !NET10_0_OR_GREATER
-		private static Dictionary<int, IReadOnlyList<string>>? _characters;
+		private static Dictionary<int, IReadOnlyList<string>>
+#if Nullable
+		?
+#endif
+		_characters;
 #endif
 
 		/// <summary>
@@ -32,7 +38,19 @@ namespace UnidecodeSharpFork
 #endif
 				is null)
 				{
-					using Stream? stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{nameof(UnidecodeSharpFork)}.{_unicodeFile}") ?? throw new FileNotFoundException(null, _unicodeFile);
+#if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
+					using
+#endif
+					Stream
+#if NET5_0_OR_GREATER
+					?
+#endif
+					stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{nameof(UnidecodeSharpFork)}.{_unicodeFile}")
+#if NET5_0_OR_GREATER
+					?? throw new FileNotFoundException(null, _unicodeFile);
+#else
+					;
+#endif
 
 #if NET10_0_OR_GREATER
 					field
@@ -45,19 +63,23 @@ namespace UnidecodeSharpFork
 #else
 					new Dictionary<int, IReadOnlyList<string>>();
 #endif
+
+#if !NETSTANDARD2_1_OR_GREATER && !NET5_0_OR_GREATER
+					stream.Dispose();
+#endif
 				}
 
-
+				return
 #if NET10_0_OR_GREATER
-				return field;			
+				field;			
 #else
-				return _characters;
+				_characters;
 #endif
 			}
 		}
 
 #if NET10_0_OR_GREATER
-		#pragma warning disable IDE0055 // Fix formatting
+#pragma warning disable IDE0055 // Fix formatting
 
 		extension(string? input)
 		{
@@ -76,22 +98,30 @@ namespace UnidecodeSharpFork
 #if NET10_0_OR_GREATER
 		public string Unidecode()
 #else
-		public static string Unidecode(this string? input)
+		public static string Unidecode(this string
+#if Nullable
+		?
+#endif
+		input)
 #endif
 		{
 			return string.IsNullOrEmpty(input) ? string.Empty : input.All(x => x < 0x80)
 				? input
-				: string.Join(string.Empty, input.Select(c => c < 0x80 ? c.ToString() : Characters.TryGetValue(c >> 8, out IReadOnlyList<string>? transliterations) ? transliterations[c & 0xff] : default));
+				: string.Join(string.Empty, input.Select(c => c < 0x80 ? c.ToString() : Characters.TryGetValue(c >> 8, out IReadOnlyList<string>
+#if Nullable
+		?
+#endif
+				transliterations) ? transliterations[c & 0xff] : default));
 		}
 
 #if NET10_0_OR_GREATER
 		}
 
-		#pragma warning restore IDE0055 // Fix formatting
+#pragma warning restore IDE0055 // Fix formatting
 #endif
 
 #if NET10_0_OR_GREATER
-		#pragma warning disable IDE0055 // Fix formatting
+#pragma warning disable IDE0055 // Fix formatting
 
 		extension(char c)
 		{
@@ -115,7 +145,11 @@ namespace UnidecodeSharpFork
 		{
 			return c < 0x80
 				? c.ToString()
-				: Characters.TryGetValue(c >> 8, out IReadOnlyList<string>? transliterations) ? transliterations[c & 0xff] : string.Empty;
+				: Characters.TryGetValue(c >> 8, out IReadOnlyList<string>
+#if Nullable
+		?
+#endif
+				transliterations) ? transliterations[c & 0xff] : string.Empty;
 		}
 
 #if NET10_0_OR_GREATER
